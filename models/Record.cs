9a87d01b1c58852b17ff22a8a3d67e6453d803ae
Cristina <cristinac.vaillant@gmail.com>
2021-10-29 19:44:00 +0200

    Representado un registro en el sistema close #17

---
+++ models/Record.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SearchFoodPetStore.Models
+{
+    class Record
+    {
+        private int id_record;
+        private int id_user;
+        private int id_food;
+        private int id_store;
+        private DateTime date;
+        private Opinion opinionFood;
+
+        public Record(int id_record, int id_user, int id_food, int id_store, DateTime date, Opinion opinionFood)
+        {
+            this.id_record = id_record;
+            this.id_user = id_user;
+            this.id_food = id_food;
+            this.id_store = id_store;
+            this.date = date;
+            this.opinionFood = opinionFood;
+        }
+    }
+}
