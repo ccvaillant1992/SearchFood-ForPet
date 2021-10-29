88cdf59b6ae4873c2d39b8ea7da2d23b335bb41b
Cristina <cristinac.vaillant@gmail.com>
2021-10-29 19:41:06 +0200

    Representada la información que posee una tienda close #15

---
+++ models/Store.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+
+namespace SearchFoodPetStore.Models
+{
+    class Store
+    {
+        private int id_store;
+        private string name;
+        private string direction;
+        private string city;
+        private string phoneNumeber;
+        private List<Food_Store> foods;
+
+        public Store(int id_store, string name, string direction, string city, string phoneNumeber, List<Food_Store> listFoods)
+        {
+            this.id_store = id_store;
+            this.name = name;
+            this.direction = direction;
+            this.city = city;
+            this.phoneNumeber = phoneNumeber;
+            this.foods = listFoods;
+        }
+    }
+}
+
+
