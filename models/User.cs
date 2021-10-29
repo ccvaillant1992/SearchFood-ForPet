b52b114f7a2b03091bcd7b215ea888c5da0a5697
Cristina <cristinac.vaillant@gmail.com>
2021-10-29 19:39:55 +0200

    Representada la información de un usuario close #14

---
+++ models/User.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+namespace SearchFoodPetStore.Models
+{
+    class User
+    {
+        private int id_user;
+        private string name;
+        private string emailUser;
+        private string location;
+        private List<Pet> listPets;
+        private List<Evaluation> evaluations;
+
+        public User(int id_User, string name, string emailUser, string location, List<Pet> listPets)
+        {
+            this.id_user = id_User;
+            this.name = name;
+            this.emailUser = emailUser;
+            this.location = location;
+            this.listPets = listPets;
+        }
+    }
+}
+
+
