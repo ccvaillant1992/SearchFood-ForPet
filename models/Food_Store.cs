efb119d01633527ddb778ae763b622099097fecc
Cristina <cristinac.vaillant@gmail.com>
2021-10-29 19:46:24 +0200

    Representada una entidad para obtener el precio de un alimento en una tienda close #21

---
+++ models/Food_Store.cs
@@ -0,0 +1,17 @@
+﻿
+namespace SearchFoodPetStore.Models
+{
+    class Food_Store
+    {
+        private int id_store;
+        private int id_food;
+        private double price;
+
+        public Food_Store(int id_store, int id_food, double price)
+        {
+            this.id_store = id_store;
+            this.id_food = id_food;
+            this.price = price;
+        }
+    }
+}
