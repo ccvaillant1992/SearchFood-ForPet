743e4564e7cc5daaa4bf39b5c64c9f5560bc25f6
Cristina <cristinac.vaillant@gmail.com>
2021-11-13 22:41:14 +0100

    Añadida la implementación de la entidad Registro close #17

---
+++ SearchFood-ForPet/Models/Record.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SearchFood_ForPet.Models
+{
+    public class Record
+    {
+        private int id_record;
+        private int id_user;
+        private int id_food;
+        private DateTime date;
+
+
+        public Record(int id_record, int id_user, int id_food, DateTime date)
+        {
+            this.id_record = id_record;
+            this.id_user = id_user;
+            this.id_food = id_food;
+            this.date = date;
+
+        }
+    }
+}
