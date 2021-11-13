41d4a7ba4e6c8e292081dae918ce55f93e1fd2ea
Cristina <cristinac.vaillant@gmail.com>
2021-11-13 22:21:32 +0100

    Creada e Implementada la estructura del proyecto a desarrollar close #20

---
+++ SearchFood-ForPet/Controllers/SearchFood_ForPetController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using SearchFood_ForPet.Models;
+
+
+namespace SearchFood_ForPet.Controllers
+{
+    [ApiController]
+    [Route("foods")]
+    public class SearchFood_ForPetController : ControllerBase
+    {
+
+        private readonly ILogger<SearchFood_ForPetController> _logger;
+
+        public SearchFood_ForPetController(ILogger<SearchFood_ForPetController> logger)
+        {
+            _logger = logger;
+
+        }
+
+        [Route("recomendations")]
+     
+        [Route("predictions")]
+       
+    }
+}
---
+++ SearchFood-ForPet/Startup.cs
@@ -0,0 +1,71 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using Microsoft.OpenApi.Models;
+
+namespace SearchFood_ForPet
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            AddSwagger(services);
+        }
+
+        private void AddSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                var groupName = "v1";
+
+                options.SwaggerDoc(groupName, new OpenApiInfo
+                {
+                    Title = $"SearchFood-ForPet {groupName}",
+                    Version = groupName,
+                    Description = "SearchFood-ForPet API",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "SearchFood-ForPet Company",
+                        Email = string.Empty,
+                        Url = new Uri("https://SearchFood-ForPet.com/"),
+                    }
+                });
+            });
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "SearchFood-ForPet API V1");
+            });
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
