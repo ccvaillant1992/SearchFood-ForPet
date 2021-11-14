1a1bcf298aea809b2b61114dcf2e3c6d351ce207
Cristina <cristinac.vaillant@gmail.com>
2021-11-14 03:58:13 +0100

    Añadida la implementación del endpoint AñadirFood en el sistema close #26, close #5

--- SearchFood-ForPet/Controllers/SearchFood_ForPetController.cs
+++ SearchFood-ForPet/Controllers/SearchFood_ForPetController.cs
@@ -11,7 +11,10 @@ namespace SearchFood_ForPet.Controllers
     public class SearchFood_ForPetController : ControllerBase
     {
 
+
         private readonly ILogger<SearchFood_ForPetController> _logger;
+
+
 
         public SearchFood_ForPetController(ILogger<SearchFood_ForPetController> logger)
         {
@@ -19,9 +22,39 @@ namespace SearchFood_ForPet.Controllers
 
         }
 
+        [HttpGet]
+        public List<Food> ListFoods()
+        {
+            List<Food> foods = new List<Food>();
+            Food newFood = new Food(1, "Taste Of The Wild", "Pacific Stream", petBreedSize.medium, "Taste of the Wild Pacific Stream....", "", foodKind.withprotein, petSpecies.dog, foodtaste.salmon);
+            foods.Add(newFood);
+            return foods;
+        }
+
+        [HttpPost]
+        public IActionResult AddFoods(Food food)
+        {
+            List<Food> foods = new List<Food>();
+            foods.Add(food);
+            return Ok();
+        }
+
         [Route("recomendations")]
-     
+        [HttpGet]
+        public IActionResult GetFoodRecomendations()
+        {
+
+            return Ok();
+
+        }
+
         [Route("predictions")]
-       
+        [HttpGet]
+        public IActionResult GetFoodPredictions()
+        {
+
+            return Ok();
+
+        }
     }
 }
