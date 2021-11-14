375d00e181f7ab5230629079fa77b00b00d4a5c1
Cristina <cristinac.vaillant@gmail.com>
2021-11-14 06:10:24 +0100

    Añadidas las clases de que contendrán los tests de los endpoints de Recomendación y Predicción close #31

---
+++ SearchFood-ForPet.Tests/PredictionsTests.cs
@@ -0,0 +1,24 @@
+﻿using SearchFood_ForPet.Controllers;
+using Xunit;
+using Microsoft.Extensions.Logging;
+
+
+namespace SearchFood_ForPet.Tests
+{
+    public class PredictionsTests
+    {
+        private readonly ILogger<SearchFood_ForPetController> loggerMock;
+        private readonly SearchFood_ForPetController searchFood_ForPetController;
+
+        [Fact]
+        public void GivenValidRequest_predictions_WhenExecutesGetFoodPredictionsThenReturnOkObject()
+        {
+            //arrange
+
+            //act
+
+            // Assert
+
+        }
+    }
+}
