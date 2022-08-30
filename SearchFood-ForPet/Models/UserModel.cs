091ddbbb914a1d1657a13f1721104f8825807313
ccvaillant1992 <cristinac.vaillant@gmail.com>
2022-08-30 14:43:29 +0200

    Implementada la entidad Usuario close #14

---
+++ SearchFood-ForPet/Models/UserModel.cs
@@ -0,0 +1,21 @@
+﻿namespace SearchFoodForPet.Models
+{
+    public class UserModel
+    {
+        public int IdUser { get; }
+        public string Name { get; set; }
+        public string EmailUser { get; set; }
+        public string Location { get; set; }
+        public List<PetModel> ListPets { get; set; }
+
+        public UserModel(int idUser, string name, string emailUser, string location, List<PetModel> listPets)
+        {
+           IdUser = idUser;
+           Name = name;
+           EmailUser = emailUser;
+           Location = location;
+           ListPets = listPets;
+        }
+
+    }
+}
