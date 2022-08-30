c93fe9d4616e6fb3d1f9099f3dfc8015ebce8094
ccvaillant1992 <cristinac.vaillant@gmail.com>
2022-08-30 14:47:28 +0200

    Implementada la entidad Alimento close #16

---
+++ SearchFood-ForPet/Models/FoodModel.cs
@@ -0,0 +1,43 @@
+﻿namespace SearchFoodForPet.Models
+{
+    public enum DietType
+    {
+        Natural = 1,
+        NonCereals = 2,
+        NonGluten = 3,
+        WithProtein = 4
+    };
+    public enum FoodKind
+    {
+        Dry = 1,
+        Wet = 2,
+        Raw = 3,
+        Homemade = 4
+    };
+    public enum FoodTaste
+    {
+        Vegetable = 1,
+        Tuna = 2,
+        Chicken = 3,
+        Pork = 4
+    };
+    public class FoodModel
+    {
+        public int IdFood { get; }
+        public PetBreedSize Size { get; }
+        public PetHealthCondition HealthCondition { get; }
+        public DietType Diet { get; set; }
+        public FoodKind Kind { get; set; }
+        public FoodTaste Taste { get; set; }
+
+        public FoodModel(int idFood, PetBreedSize size, PetHealthCondition condition, DietType diet, FoodKind kind, FoodTaste taste)
+        {
+            IdFood = idFood;
+            Size = size;
+            HealthCondition = condition;
+            Diet = diet;
+            Kind = kind;
+            Taste = taste;
+        }
+    }
+}
